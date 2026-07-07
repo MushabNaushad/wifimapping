a46b2a5d39c4c0a43b359b68dfac35d64846c99f
MushabNaushad <ahamedmushab9@gmail.com>
2026-07-07 23:31:03 +0530

    initialization

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+
+
+
+
+void app_main(void){
+    
+}
